b4f7173e330a770512b46f6a8c82916d5aaf3bbd
zluis17 <zoeluquintero@gmail.com>
2024-11-18 21:45:12 -0500

    frontEnd_solo

--- BackEND/controllers/config.js
+++
@@ -1,5 +0,0 @@
-export const {
-    PORT = 3000,
-    SALT_ROUNDS=10,
-    SECRET_JWT_KET='this-is-an-awesome-secret-key',
-} = process.env
--- BackEND/database/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const db = new Sequelize('ctxl', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: console.log,
-});
-
-export default db
